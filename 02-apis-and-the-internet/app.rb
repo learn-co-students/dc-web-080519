377a2ca14d18a23dc1e90916a211e99e96ccb6fe
Paul Nicholsen <paul.nicholsen@flatironschool.com>
2019-08-06 11:32:17 -0400

    postlecture 02

--- 02-apis-and-the-internet/app.rb
+++ 02-apis-and-the-internet/app.rb
@@ -4,3 +4,49 @@ require 'pry'
 
 GOOGLE_BOOKS_API_BASE_URL = "https://www.googleapis.com/books/v1/volumes?q="
 
+
+# response = make_request("www.reddit.com/.json")
+# reddit_hash = parse_into_hash(response)
+
+# response = RestClient.get(url)
+# json = JSON.parse(response)
+
+
+def get_search_terms
+    # get a search term from a user
+    puts "What would you like to search for?"
+    search_terms = gets.chomp
+end
+
+# find the top ten books about that search term
+    # build the url
+def build_url(search_terms)
+    url = GOOGLE_BOOKS_API_BASE_URL + search_terms.gsub(" ", "+")
+end
+
+def get_book_json(url)
+    response = RestClient.get(url)
+    json = JSON.parse(response)
+end
+
+def parse_books(json)
+    # get first ten items
+    first_ten = json["items"][0...10]
+    first_ten.each do |book|
+        puts book["volumeInfo"]["title"]
+        puts book["volumeInfo"]["authors"]
+        puts "*********"
+    end
+
+end
+
+def run
+    search_terms = get_search_terms
+    url = build_url(search_terms)
+    json = get_book_json(url)
+    parse_books(json)
+end
+
+run
+
+# for each item print title and author
--- 02-apis-and-the-internet/donow.rb
+++ 02-apis-and-the-internet/donow.rb
@@ -15,22 +15,80 @@
 # the block evaluates as True
 
 nums = (1..20).to_a #creates an array of all numbers from 1-20
-
+# print nums
 # 1.  Create an array of numbers where each new number is three times as big as
 # its original number (e.g., [3, 6, 9])
 
+def times_three(nums)
+    answer = []
+    nums.each do |num|
+        answer << num * 3
+    end
+    return answer
+end
+
+def times_three_map(nums)
+    nums.map do |num|
+        num * 3
+    end
+end
+
+# print times_three_map(nums)
 
 # 2.  Find the first number that is divisible by 7
 
+def divisible_by_seven(nums)
+    nums.each do |num|
+        if num % 7 == 0
+            return num
+        end
+    end
+end
+
+# print divisible_by_seven(nums)
+
+def find_divisible_by_seven(nums)
+    answer = nums.find do |number|
+        number % 7 == 0
+    end
+    return answer
+end
+
+returned_answer = find_divisible_by_seven(nums)
 
 # 3.  Find all numbers that are divisible by 7
 
+def find_all_divisible_by_seven(nums)
+    nums.select do |number|
+        number % 7 == 0
+    end
+end
+
+# print find_all_divisible_by_seven(nums)
 
 # 4.  Find the first number that is divisible by 7 AND greater than 10
 
+def find_big_divisible_by_seven(nums)
+    # nums.find do |num|
+    #     num % 7 == 0 && num > 10
+    # end
+    find_all_divisible_by_seven(nums).find do |num|
+        num > 10
+    end
+end
+
+# print find_big_divisible_by_seven(nums)
 
 # Bonus:
 
 
 # 5.  Create an array of the squares (the number times itself) of all numbers
 # that are divisible by 7
+
+def multiple_seven_squares(nums)
+    nums.select do |num|
+        num % 7 == 0
+    end.map {|num| num ** 2}
+end
+
+print multiple_seven_squares(nums)
