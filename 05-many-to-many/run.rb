d56f27dfb49b847421ead151d4f5bc0f60ff167a
Paul Nicholsen <paul.nicholsen@flatironschool.com>
2019-08-08 14:58:41 -0400

    prelecture 05

---
+++ 05-many-to-many/run.rb
@@ -0,0 +1,15 @@
+require "pry"
+require_relative "./tweet.rb"
+require_relative "./user.rb"
+
+coffee_dad = User.new("Coffee Dad")
+soda_mom = User.new("Soda Mom")
+
+tweet1 = Tweet.new("I love coffee", coffee_dad)
+tweet2 = Tweet.new("Making coffee", coffee_dad)
+tweet3 = Tweet.new("I need coffee", coffee_dad)
+tweet4 = Tweet.new("Life is a meaningless void", coffee_dad)
+tweet5 = Tweet.new("I need tea", soda_mom)
+tweet6 = Tweet.new("I like tea", soda_mom)
+tweet7 = Tweet.new("Life is a wonderous place", soda_mom)
+
