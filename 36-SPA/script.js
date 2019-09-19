c41fc10dd8022373ada4d418ba0e3ca483ab90a2
Bruno Garcia Gonzalez <brunogarciagonzalez@outlook.com>
2019-09-19 13:01:52 -0400

    add 36 complete

--- 36-SPA/script.js
+++ 36-SPA/script.js
@@ -17,14 +17,45 @@ document.addEventListener("DOMContentLoaded", function(){
   console.log('DOM loaded')
 
   gottaFetchEmAll();
-
+  initializeForm();
 
 });
+
+
+function initializeForm(){
+  let form = document.querySelector("form");
+  form.addEventListener("submit", handleFormSubmission)
+}
+
+function handleFormSubmission(e){
+  e.preventDefault()
+  let formData = {
+    name: document.querySelector("#name-input").value,
+    sprite: document.querySelector("#sprite-input").value
+  }
+
+  fetch("http://localhost:3000/pokemons", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(formData) //formData in string format
+  })
+  .then(res => {
+    renderPokemon(formData)
+  })
+  .catch(error => {
+    alert("Server down, please try again later.")
+  })
+
+}
+
 
 function renderPokemon(pmonObj){
   // initialize pokemon div, with "card" class
   let pokemonCard = document.createElement("div");
   pokemonCard.classList.add("card")
+  pokemonCard.id = `pokemon-${pmonObj.id}`;
 
   // work on the header for the card
   let pokemonHeader = document.createElement("h3");
@@ -38,9 +69,33 @@ function renderPokemon(pmonObj){
   // add h3, img to the card
   pokemonCard.append(pokemonHeader, pokemonImage)
 
+
+  // add delete functionality to card
+  pokemonCard.addEventListener("click", deletePokemon)
+
   // add card to container (container is already on the page)
   let pokemonContainer = document.querySelector("#pokemon-container")
   pokemonContainer.appendChild(pokemonCard)
+}
+
+function deletePokemon(e){
+  // delete pokemon
+
+  let card = e.currentTarget;
+  let id = card.id.split("-")[1]
+  fetch(`http://localhost:3000/pokemons/${id}`, {
+    method: "DELETE"
+  })
+  .then(res => {
+    // debugger
+    card.remove()
+
+  })
+  .catch(error => {
+    alert("Server down, please try again later.")
+  })
+
+
 }
 
 function gottaFetchEmAll(){
